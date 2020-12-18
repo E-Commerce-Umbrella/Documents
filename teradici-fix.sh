b5ef4b0970499efd8eaac44bac730a714e3f67c9
Dave Tacker <david.tacker@gmail.com>
2020-12-18 12:34:03 -1000

    Create teradici-fix.sh

---
+++ teradici-fix.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ------------------------------------------------------
+# Fix for Teradici
+# 
+# This is a fix for:
+# "PCoIP Agent has no available licenses to launch the remote session."
+# execute this script from terminal: 
+# ------------------------------------------------------
+
+find /var/lib/pcoip-agent/licensing -type f -exec rm {} \;
+apt-key del 4E45878267D7ADA8
+
+# Until there is a 20.04 version of the repo, we'll have to check...
+if [ $(lsb_release -cs) = "focal" ]; then
+  release=bionic
+else
+  release=$(lsb_release -cs)
+fi
+
+wget -O teradici-repo-latest.deb https://downloads.teradici.com/ubuntu/teradici-repo-${release}-latest.deb
+apt install --reinstall ./teradici-repo-latest.deb
+apt update
+apt install pcoip-agent-standard
+
+pcoip-register-host --registration-code=EY81ZSNSYGSC@4016-A932-615E-F0EB
+reboot
