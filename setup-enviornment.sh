7ed76fd9c6eb44f26c013de23e6c9cb7515d5699
Dave Tacker <david.tacker@gmail.com>
2020-12-18 09:23:03 -1000

    Update setup-enviornment.sh

--- setup-enviornment.sh
+++ setup-enviornment.sh
@@ -1,19 +1,22 @@
 #bin/bash
 
+# Name the log file
+LOG_FILE=setup-enviornment.log
+
 # Create the log file
-touch setup-enviornment.log
+touch ${LOG_FILE}
 
 # Clear the log file
-> setup-enviornment.log
+> ${LOG_FILE}
 
 # Clear console for pretty output
 clear
 
 echo "Adding repositories to APT"
-curl -sL https://deb.nodesource.com/setup_10.x | bash | tee setup-enviornment.log
+curl -sL https://deb.nodesource.com/setup_10.x 3>&1 1>>${LOG_FILE} 2>&1 | bash
 
 echo "Updating APT..."
-apt update | tee setup-enviornment.log
+apt update 3>&1 1>>${LOG_FILE} 2>&1
 
 echo "Installing packages..."
-apt install -y apache2 php libapache2-mod-php php-pear php-mysql apache2-utils mysql-server curl php-cli php-mbstring php-curl git unzip | tee setup-enviornment.log
+apt install -y apache2 php libapache2-mod-php php-pear php-mysql apache2-utils mysql-server curl php-cli php-mbstring php-curl git unzip | 3>&1 1>>${LOG_FILE} 2>&1
