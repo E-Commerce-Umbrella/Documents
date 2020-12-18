d993cdcc4a2d596f6e74f3c6767d9e83483f54db
Dave Tacker <david.tacker@gmail.com>
2020-12-18 09:05:47 -1000

    Create setup-enviornment.sh

---
+++ setup-enviornment.sh
@@ -0,0 +1,6 @@
+#bin/bash
+
+echo "foo"
+
+#sudo apt-get update
+#sudo apt-get install -y apache2 php libapache2-mod-php php-pear php-mysql apache2-utils mysql-server curl php-cli php-mbstring php-curl git unzip
