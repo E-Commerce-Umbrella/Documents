94ab3f3adf829a7fcd3a1393fee3cd6502e33150
kneeki <david.tacker@gmail.com>
2021-05-05 10:03:36 -1000

    Revert "Delete teradici.sh"
    This reverts commit 0174b6280a3dff52c5bcfa69cea871afed3650b9.

---
+++ teradici.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# ------------------------------------------------------
+# Execute this script from terminal: 
+# curl -o- https://raw.githubusercontent.com/E-Commerce-Umbrella/Documents/main/teradici.sh?token=AAHXDSPJ3A5MYZKMNKXHSH274ZUOE | sudo -E bash -
+# ------------------------------------------------------
+
+find /var/lib/pcoip-agent/licensing -type f -exec rm {} \;
+apt-key del 4E45878267D7ADA8
+
+# Until there is a 20.04 version of the repo, we'll have to check...
+if [ $(lsb_release -cs) = "focal" ]; then
+  release=bionic
+else
+  release=$(lsb_release -cs)
+fi
+
+wget -O teradici-repo-latest.deb https://downloads.teradici.com/ubuntu/teradici-repo-${release}-latest.deb
+apt install --reinstall ./teradici-repo-latest.deb
+apt update
+apt install pcoip-agent-graphics pcoip-agent-standard tasksel
+
+pcoip-register-host --registration-code=EY81ZSNSYGSC@4016-A932-615E-F0EB
+tasksel
